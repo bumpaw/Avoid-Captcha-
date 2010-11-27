1ced931211d54c37e4f644bb8caee4e73ae93c37
John Powell <mermentau@gmail.com>
2010-11-27 00:02:30 -0600

    The javascript is all set now.
    Signed-off-by: John Powell <mermentau@gmail.com>

--- cap_sub/token.php
+++ cap_sub/token.php
@@ -1,10 +1,14 @@
 <?php
-// <!-- phpDesigner :: Timestamp [11/23/2010 11:18:57 PM] -->
+// <!-- phpDesigner :: Timestamp [11/26/2010 10:48:37 AM] -->
 /**
- *http://15daysofjquery.com/safer-contact-forms-without-captchas/11/
- *
+ * Based on the work of Jack Born in Spring 2006
+ * http://15daysofjquery.com/safer-contact-forms-without-captchas/11/
+ * Updated link: 11/25/2010
+ * Above is now a bum link but a slightly updated version is found below
+ * http://docs.jquery.com/Tutorials:Safer_Contact_Forms_Without_CAPTCHAs
  */
 $ct = mktime();
+// Probably need way to set your own secret salt here for drupal.
 setcookie('token',md5('africa byby'.$ct), 0, '/');
 # 'Expires' in the past
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
