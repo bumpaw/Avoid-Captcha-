5f0a9be3700cbb28962000186b744292c944a305
John Powell <mermentau@gmail.com>
2013-05-20 18:38:33 -0500

    trying to come up with a patch for learning.

--- kayak.php
+++ kayak.php
@@ -1,4 +1,9 @@
 <?php
 echo "We are working on master here";
 echo 'It\'s a kayak crazy werld.';
+function likewow($splat){
+    echo "I think that the bird went . $splat";
+
+}
+likewow('boom');
 ?>
